0fc27032c37fd290da007785a148cf49f71adde5
spectre360 <spectre360@72bbbca6-d526-0410-a7d9-f06f51895060>
2016-04-26 14:10:12 +0000

    remove old testvoc scripts
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/trunk@67664 72bbbca6-d526-0410-a7d9-f06f51895060

--- apertium-spa-ita/dev/testvoc.sh
+++
@@ -1,5 +0,0 @@
-echo "==Spanish->Italian============================";
-sh inconsistency.sh es-it > /tmp/es-it.testvoc; sh inconsistency-summary.sh /tmp/es-it.testvoc es-it
-echo ""
-echo "==Italian->Spanish=============================";
-bash inconsistency.sh it-es > /tmp/it-es.testvoc; bash inconsistency-summary.sh /tmp/it-es.testvoc it-es
